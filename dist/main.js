18189dc9734497df98f4b3d4ce9785e1d3d48937
zhangchao <529743595@qq.com>
2020-07-01 22:35:29 +0800

    Use Class

--- dist/main.js
+++ dist/main.js
@@ -1,105 +1,115 @@
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => [...document.querySelectorAll(selector)];
-const Model = {
-    data: {
-        list: []
-    },
-    getData: () => new Promise((resolve, reject) => {
-        const data = JSON.parse(window.localStorage.getItem('list') || '[]');
-        Model.data.list = data;
-        resolve(data);
-    }),
-    updateData: ({ id, value }) => new Promise((resolve, reject) => {
-        const data = Model.data.list;
-        const currentData = data.find(v => v.id === id);
-        currentData && (currentData.value = value);
-        window.localStorage.setItem('list', JSON.stringify(data));
-        Model.data.list = data;
-        resolve(data);
-    }),
-    saveData: (value) => new Promise((resolve, reject) => {
-        var _a;
-        const data = Model.data.list;
-        const newData = [...data, { id: (((_a = data.slice(-1)[0]) === null || _a === void 0 ? void 0 : _a.id) || 0) + 1, value }];
-        window.localStorage.setItem('list', JSON.stringify(newData));
-        Model.data.list = newData;
-        resolve(newData);
-    }),
-    deleteData: (id) => new Promise((resolve, reject) => {
-        const data = Model.data.list;
-        const newData = data.filter(v => v.id !== id);
-        window.localStorage.setItem('list', JSON.stringify(newData));
-        Model.data.list = newData;
-        resolve(newData);
-    }),
-};
-const View = {
-    el: '#app',
-    template: `
+class Model {
+    constructor() {
+        this.getData = () => new Promise((resolve, reject) => {
+            const data = JSON.parse(window.localStorage.getItem('list') || '[]');
+            this.data = data;
+            resolve(data);
+        });
+        this.updateData = ({ id, value }) => new Promise((resolve, reject) => {
+            const data = this.data;
+            const currentData = data.find(v => v.id === id);
+            currentData && (currentData.value = value);
+            window.localStorage.setItem('list', JSON.stringify(data));
+            this.data = data;
+            resolve(data);
+        });
+        this.saveData = (value) => new Promise((resolve, reject) => {
+            var _a;
+            const data = this.data;
+            const newData = [...data, { id: (((_a = data.slice(-1)[0]) === null || _a === void 0 ? void 0 : _a.id) || 0) + 1, value }];
+            window.localStorage.setItem('list', JSON.stringify(newData));
+            this.data = newData;
+            resolve(newData);
+        });
+        this.deleteData = (id) => new Promise((resolve, reject) => {
+            const data = this.data;
+            const newData = data.filter(v => v.id !== id);
+            window.localStorage.setItem('list', JSON.stringify(newData));
+            this.data = newData;
+            resolve(newData);
+        });
+    }
+}
+class View {
+    constructor() {
+        this.el = '#app';
+        this.template = `
     <div data-id="{{id}}">
       <input disabled type="text" value="{{value}}">
       <button class="edit">编辑</button>
       <button class="update" style="display: none;">保存</button>
       <button class="delete">删除</button>
     </div>
-  `,
-    render: () => {
-        $('.content').innerHTML = Model.data.list.map((item) => View.template.replace('{{value}}', item.value).replace('{{id}}', item.id.toString())).join('');
-    },
-};
-const Controller = {
-    init: () => {
-        Model.getData().then(() => {
-            View.render();
-        });
-        Controller.bindEvents();
-    },
-    events: [
-        { type: 'click', selector: '.add', fnName: 'handleAdd' },
-        { type: 'click', selector: '.delete', fnName: 'handleDelete' },
-        { type: 'click', selector: '.edit', fnName: 'handleEdit' },
-        { type: 'click', selector: '.update', fnName: 'handleUpdate' },
-    ],
-    bindEvents: () => {
-        Controller.events.map((myEvent) => {
-            $(View.el).addEventListener(myEvent.type, (event) => {
-                const targetElement = event.target;
-                if ($$(myEvent.selector).includes(targetElement)) {
-                    Controller[myEvent.fnName](targetElement);
-                }
+  `;
+        this.render = (data) => {
+            $('.content').innerHTML = data.map((item) => this.template.replace('{{value}}', item.value).replace('{{id}}', item.id.toString())).join('');
+        };
+    }
+}
+class Controller {
+    constructor({ view, model }) {
+        this.init = () => {
+            this.model.getData().then(() => {
+                this.view.render(this.model.data);
             });
-        });
-    },
-    handleAdd() {
-        const inputElement = $('.input');
-        const value = inputElement.value;
-        if (value === '')
-            return;
-        Model.saveData(value).then(() => {
-            inputElement.value = '';
-            View.render();
-        });
-    },
-    handleDelete(targetElement) {
-        Model.deleteData(+targetElement.parentElement.getAttribute('data-id')).then(() => View.render());
-    },
-    handleEdit(targetElement) {
-        const inputElement = targetElement.previousElementSibling;
-        const saveElement = targetElement.nextElementSibling;
-        inputElement.disabled = false;
-        saveElement.style.display = 'inline-block';
-        targetElement.style.display = 'none';
-    },
-    handleUpdate(targetElement) {
-        const inputElement = targetElement.previousElementSibling.previousElementSibling;
-        const editElement = targetElement.previousElementSibling;
-        Model.updateData({
-            id: +targetElement.parentElement.getAttribute('data-id'),
-            value: inputElement.value
-        }).then(() => {
-            editElement.style.display = 'inline-block';
-            View.render();
-        });
-    },
-};
-Controller.init();
+            this.bindEvents();
+        };
+        this.events = [
+            { type: 'click', selector: '.add', fnName: 'handleAdd' },
+            { type: 'click', selector: '.delete', fnName: 'handleDelete' },
+            { type: 'click', selector: '.edit', fnName: 'handleEdit' },
+            { type: 'click', selector: '.update', fnName: 'handleUpdate' },
+        ];
+        this.bindEvents = () => {
+            this.events.map((myEvent) => {
+                $(this.view.el).addEventListener(myEvent.type, (event) => {
+                    const targetElement = event.target;
+                    if ($$(myEvent.selector).includes(targetElement)) {
+                        // @ts-ignore
+                        this[myEvent.fnName](targetElement);
+                    }
+                });
+            });
+        };
+        this.handleAdd = () => {
+            const inputElement = $('.input');
+            const value = inputElement.value;
+            if (value === '')
+                return;
+            this.model.saveData(value).then(() => {
+                inputElement.value = '';
+                this.view.render(this.model.data);
+            });
+        };
+        this.handleDelete = (targetElement) => {
+            this.model.deleteData(+targetElement.parentElement.getAttribute('data-id')).then(() => this.view.render(this.model.data));
+        };
+        this.handleEdit = (targetElement) => {
+            const inputElement = targetElement.previousElementSibling;
+            const saveElement = targetElement.nextElementSibling;
+            inputElement.disabled = false;
+            saveElement.style.display = 'inline-block';
+            targetElement.style.display = 'none';
+        };
+        this.handleUpdate = (targetElement) => {
+            const inputElement = targetElement.previousElementSibling.previousElementSibling;
+            const editElement = targetElement.previousElementSibling;
+            this.model.updateData({
+                id: +targetElement.parentElement.getAttribute('data-id'),
+                value: inputElement.value
+            }).then(() => {
+                editElement.style.display = 'inline-block';
+                this.view.render(this.model.data);
+            });
+        };
+        this.view = view;
+        this.model = model;
+        this.init();
+    }
+}
+new Controller({
+    view: new View(),
+    model: new Model()
+});
--- main.ts
+++ main.ts
@@ -2,116 +2,126 @@ interface Data {
   id: number;
   value: string;
 }
-
-const $ = (selector: string): Element => document.querySelector(selector)
-const $$ = (selector: string): Element[] => [...document.querySelectorAll(selector)]
-
-const Model: any = {
-  data: {
-    list: []
-  },
-  getData: () => new Promise((resolve, reject) => {
-    const data: Data[] = JSON.parse(window.localStorage.getItem('list') || '[]');
-    Model.data.list = data
-    resolve(data);
-  }),
-  updateData: ({id, value}: Data) => new Promise((resolve, reject) => {
-    const data: Data[] = Model.data.list
-    const currentData = data.find(v => v.id === id)
-    currentData && (currentData.value = value)
-    window.localStorage.setItem('list', JSON.stringify(data))
-    Model.data.list = data
-    resolve(data)
-  }),
-  saveData: (value: string) => new Promise((resolve, reject) => {
-    const data: Data[] = Model.data.list
-    const newData = [...data, {id: (data.slice(-1)[0]?.id || 0) + 1, value}]
-    window.localStorage.setItem('list', JSON.stringify(newData))
-    Model.data.list = newData
-    resolve(newData)
-  }),
-  deleteData: (id: number) => new Promise((resolve, reject) => {
-    const data: Data[] = Model.data.list
-    const newData: Data[] = data.filter(v => v.id !== id)
-    window.localStorage.setItem('list', JSON.stringify(newData))
-    Model.data.list = newData
-    resolve(newData)
-  }),
-}
-
-const View: any = {
-  el: '#app',
-  template: `
-    <div data-id="{{id}}">
-      <input disabled type="text" value="{{value}}">
-      <button class="edit">编辑</button>
-      <button class="update" style="display: none;">保存</button>
-      <button class="delete">删除</button>
-    </div>
-  `,
-  render: () => {
-    $('.content').innerHTML = Model.data.list.map((item: Data) => View.template.replace('{{value}}', item.value).replace('{{id}}', item.id.toString())).join('')
-  },
-}
-
 interface MyEvent {
   type: string;
   selector: string;
   fnName: string;
 }
 
-const Controller: any = {
-  init: () => {
-    Model.getData().then(() => {
-      View.render()
+const $ = (selector: string): Element => document.querySelector(selector)
+const $$ = (selector: string): Element[] => [...document.querySelectorAll(selector)]
+
+class Model {
+  data: Data[]
+  getData = () => new Promise((resolve, reject) => {
+    const data: Data[] = JSON.parse(window.localStorage.getItem('list') || '[]');
+    this.data = data
+    resolve(data);
+  })
+  updateData = ({id, value}: Data) => new Promise((resolve, reject) => {
+    const data: Data[] = this.data
+    const currentData = data.find(v => v.id === id)
+    currentData && (currentData.value = value)
+    window.localStorage.setItem('list', JSON.stringify(data))
+    this.data = data
+    resolve(data)
+  })
+  saveData = (value: string) => new Promise((resolve, reject) => {
+    const data: Data[] = this.data
+    const newData = [...data, {id: (data.slice(-1)[0]?.id || 0) + 1, value}]
+    window.localStorage.setItem('list', JSON.stringify(newData))
+    this.data = newData
+    resolve(newData)
+  })
+  deleteData = (id: number) => new Promise((resolve, reject) => {
+    const data: Data[] = this.data
+    const newData: Data[] = data.filter(v => v.id !== id)
+    window.localStorage.setItem('list', JSON.stringify(newData))
+    this.data = newData
+    resolve(newData)
+  })
+  constructor () {}
+}
+
+class View {
+  el: string = '#app'
+  template:string = `
+    <div data-id="{{id}}">
+      <input disabled type="text" value="{{value}}">
+      <button class="edit">编辑</button>
+      <button class="update" style="display: none;">保存</button>
+      <button class="delete">删除</button>
+    </div>
+  `
+  render = (data: Data[]) => {
+    $('.content').innerHTML = data.map((item: Data) => this.template.replace('{{value}}', item.value).replace('{{id}}', item.id.toString())).join('')
+  }
+}
+
+class Controller {
+  view: any
+  model: any
+  constructor ({view, model}: any) {
+    this.view = view
+    this.model = model
+    this.init()
+  }
+  init = () => {
+    this.model.getData().then(() => {
+      this.view.render(this.model.data)
     })
-    Controller.bindEvents()
-  },
-  events: [
+    this.bindEvents()
+  }
+  events: MyEvent[] = [
     {type: 'click', selector: '.add', fnName: 'handleAdd'},
     {type: 'click', selector: '.delete', fnName: 'handleDelete'},
     {type: 'click', selector: '.edit', fnName: 'handleEdit'},
     {type: 'click', selector: '.update', fnName: 'handleUpdate'},
-  ],
-  bindEvents: () => {
-    Controller.events.map((myEvent: MyEvent) => {
-      $(View.el).addEventListener(myEvent.type, (event: Event) => {
+  ]
+  bindEvents = () => {
+    this.events.map((myEvent: MyEvent) => {
+      $(this.view.el).addEventListener(myEvent.type, (event: Event) => {
         const targetElement = event.target as HTMLElement
         if ($$(myEvent.selector).includes(targetElement)) {
-          Controller[myEvent.fnName](targetElement)
+          // @ts-ignore
+          this[myEvent.fnName](targetElement)
         }
       })
     })
-  },
-  handleAdd () {
+  }
+  handleAdd = () => {
     const inputElement = $('.input') as HTMLInputElement
     const value = inputElement.value
     if (value === '') return
-    Model.saveData(value).then(() => {
+    this.model.saveData(value).then(() => {
       inputElement.value = ''
-      View.render()
+      this.view.render(this.model.data)
     })
-  },
-  handleDelete (targetElement: HTMLElement) {
-    Model.deleteData(+targetElement.parentElement.getAttribute('data-id')).then(() => View.render())
-  },
-  handleEdit (targetElement: HTMLElement) {
+  }
+  handleDelete = (targetElement: HTMLElement) => {
+    this.model.deleteData(+targetElement.parentElement.getAttribute('data-id')).then(() => this.view.render(this.model.data))
+  }
+  handleEdit = (targetElement: HTMLElement) => {
     const inputElement = targetElement.previousElementSibling as HTMLInputElement
     const saveElement = targetElement.nextElementSibling as HTMLElement
     inputElement.disabled = false
     saveElement.style.display = 'inline-block'
     targetElement.style.display = 'none'
-  },
-  handleUpdate (targetElement: HTMLElement) {
+  }
+  handleUpdate = (targetElement: HTMLElement) => {
     const inputElement = targetElement.previousElementSibling.previousElementSibling as HTMLInputElement
     const editElement = targetElement.previousElementSibling as HTMLElement
-    Model.updateData({
+    this.model.updateData({
       id: +targetElement.parentElement.getAttribute('data-id'),
       value: inputElement.value
     }).then(() => {
       editElement.style.display = 'inline-block'
-      View.render()
+      this.view.render(this.model.data)
     })
-  },
+  }
 }
-Controller.init()
+
+new Controller({
+  view: new View(),
+  model: new Model()
+})
